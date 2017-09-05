f62ba3b655715f555f622e1b8467162c77970c09
Levin <1837307557@qq.com>
2017-09-05 18:54:11 +0800

    全局异常捕获处理

--- battcn-manage/src/main/java/com/battcn/platform/pojo/message/ApiResult.java
+++ battcn-manage/src/main/java/com/battcn/platform/pojo/message/ApiResult.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
-import com.sun.xml.internal.ws.api.message.Packet;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import org.springframework.http.HttpStatus;
