ec37a406b292021e292216b742dd1accef55516e
Levin <1837307557@qq.com>
2017-09-05 18:48:00 +0800

    添加分组显示的支持

--- battcn-framework-starter-swagger/src/main/java/com/battcn/framework/monitor/swagger/SwaggerAutoConfiguration.java
+++ battcn-framework-starter-swagger/src/main/java/com/battcn/framework/monitor/swagger/SwaggerAutoConfiguration.java
@@ -1,88 +1,110 @@
 package com.battcn.framework.monitor.swagger;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMethod;
-import com.battcn.framework.monitor.swagger.properties.ApiInfoProperties;
-import com.battcn.framework.monitor.swagger.properties.DocketProperties;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableConfigurationProperties(value = {SwaggerProperties.class})
-@ConditionalOnProperty(name = "swagger.enable")
+@ConditionalOnProperty(name = {"swagger.enabled"})
 @EnableSwagger2
-public class SwaggerAutoConfiguration {
+public class SwaggerAutoConfiguration implements BeanFactoryAware {
+    private BeanFactory beanFactory;
 
-	@Autowired
-	SwaggerProperties properties;
-	
-	@Bean
-    public Docket petApi() {
-		DocketProperties docket = properties.getDocket();
-        List<ResponseMessage> messages = new ArrayList<>();
-        ResponseMessage message1 = new ResponseMessageBuilder().code(200).message("操作成功").responseModel(new ModelRef("操作成功")).build();
-        ResponseMessage message2 = new ResponseMessageBuilder().code(400).message("非法请求").responseModel(new ModelRef("非法请求")).build();
-        ResponseMessage message3 = new ResponseMessageBuilder().code(501).message("如请求路径拼写不正确").responseModel(new ModelRef("如请求路径拼写不正确")).build();
-        ResponseMessage message4 = new ResponseMessageBuilder().code(502).message("服务器过载引起的错误").responseModel(new ModelRef("服务器过载引起的错误")).build();
-        messages.add(message1);
-        messages.add(message2);
-        messages.add(message3);
-        messages.add(message4);
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .groupName(docket.getGroupName())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage(docket.getBasePackage()))
-                .paths(PathSelectors.any())
-                .build()
-                .pathMapping("/")
-                .directModelSubstitute(LocalDate.class, String.class)
-                .genericModelSubstitutes(ResponseEntity.class)
-                //.alternateTypeRules(
-                //        newRule(typeResolver.resolve(DeferredResult.class,
-                //                typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
-                //                typeResolver.resolve(WildcardType.class)))
-                .useDefaultResponseMessages(false)
-                .globalResponseMessage(RequestMethod.POST, messages)
-                //.securitySchemes(apiKeys())
-                //.securityContexts(securityContext())
-                //.globalOperationParameters(newArrayList(new ParameterBuilder().name("someGlobalParameter").description("Description of someGlobalParameter").modelRef(new ModelRef("string")).parameterType("query").required(true).build()))
-                //.additionalModels(typeResolver.resolve(AdditionalModel.class))
-                ;
+    public SwaggerAutoConfiguration() {
     }
 
-    private ApiInfo apiInfo() {
-    	ApiInfoProperties apiInfo = properties.getApiInfo();
-    	com.battcn.framework.monitor.swagger.properties.Contact contact = apiInfo.getContact();
-        return new ApiInfoBuilder()
-                .title(apiInfo.getTitle())
-                .description(apiInfo.getDescription())
-                .termsOfServiceUrl(apiInfo.getTermsOfServiceUrl())
-                .contact(new Contact(contact.getName(), contact.getUrl(), contact.getEmail()))
-                .version(apiInfo.getVersion())
-                .license(apiInfo.getLicense())
-                .licenseUrl(apiInfo.getLicenseUrl())
-                .build();
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
     }
-	
-	
-	
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SwaggerProperties swaggerProperties() {
+        return new SwaggerProperties();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public List<Docket> createRestApi(SwaggerProperties swaggerProperties) {
+        ConfigurableBeanFactory configurableBeanFactory = (ConfigurableBeanFactory) this.beanFactory;
+        if (swaggerProperties.getDocket().size() == 0) {
+            ApiInfo apiInfo = (new ApiInfoBuilder()).title(swaggerProperties.getTitle()).description(swaggerProperties.getDescription()).version(swaggerProperties.getVersion()).license(swaggerProperties.getLicense()).licenseUrl(swaggerProperties.getLicenseUrl()).contact(new Contact(swaggerProperties.getContact().getName(), swaggerProperties.getContact().getUrl(), swaggerProperties.getContact().getEmail())).termsOfServiceUrl(swaggerProperties.getTermsOfServiceUrl()).build();
+            if (swaggerProperties.getBasePath().isEmpty()) {
+                swaggerProperties.getBasePath().add("/**");
+            }
+
+            List<Predicate<String>> basePath = new ArrayList<>();
+            Iterator bases = swaggerProperties.getBasePath().iterator();
+
+            while (bases.hasNext()) {
+                String path = (String) bases.next();
+                basePath.add(PathSelectors.ant(path));
+            }
+
+            List<Predicate<String>> excludePath = new ArrayList<>();
+            Iterator excludes = swaggerProperties.getExcludePath().iterator();
+
+            while (excludes.hasNext()) {
+                String path = (String) excludes.next();
+                excludePath.add(PathSelectors.ant(path));
+            }
+
+            Docket docket = (new Docket(DocumentationType.SWAGGER_2)).apiInfo(apiInfo).select().apis(RequestHandlerSelectors.basePackage(swaggerProperties.getBasePackage())).paths(Predicates.and(Predicates.not(Predicates.or(excludePath)), Predicates.or(basePath))).build();
+            configurableBeanFactory.registerSingleton("defaultDocket", docket);
+            return null;
+        } else {
+            List<Docket> docketList = new LinkedList<>();
+            Iterator dockets = swaggerProperties.getDocket().keySet().iterator();
+
+            while (dockets.hasNext()) {
+                String groupName = (String) dockets.next();
+                SwaggerProperties.DocketInfo docketInfo = swaggerProperties.getDocket().get(groupName);
+                ApiInfo apiInfo = (new ApiInfoBuilder()).title(docketInfo.getTitle().isEmpty() ? swaggerProperties.getTitle() : docketInfo.getTitle()).description(docketInfo.getDescription().isEmpty() ? swaggerProperties.getDescription() : docketInfo.getDescription()).version(docketInfo.getVersion().isEmpty() ? swaggerProperties.getVersion() : docketInfo.getVersion()).license(docketInfo.getLicense().isEmpty() ? swaggerProperties.getLicense() : docketInfo.getLicense()).licenseUrl(docketInfo.getLicenseUrl().isEmpty() ? swaggerProperties.getLicenseUrl() : docketInfo.getLicenseUrl()).contact(new Contact(docketInfo.getContact().getName().isEmpty() ? swaggerProperties.getContact().getName() : docketInfo.getContact().getName(), docketInfo.getContact().getUrl().isEmpty() ? swaggerProperties.getContact().getUrl() : docketInfo.getContact().getUrl(), docketInfo.getContact().getEmail().isEmpty() ? swaggerProperties.getContact().getEmail() : docketInfo.getContact().getEmail())).termsOfServiceUrl(docketInfo.getTermsOfServiceUrl().isEmpty() ? swaggerProperties.getTermsOfServiceUrl() : docketInfo.getTermsOfServiceUrl()).build();
+                if (docketInfo.getBasePath().isEmpty()) {
+                    docketInfo.getBasePath().add("/**");
+                }
+
+                List<Predicate<String>> basePath = new ArrayList<>();
+                Iterator bases = docketInfo.getBasePath().iterator();
+
+                while (bases.hasNext()) {
+                    String path = (String) bases.next();
+                    basePath.add(PathSelectors.ant(path));
+                }
+
+                List<Predicate<String>> excludePath = new ArrayList<>();
+                Iterator excludes = docketInfo.getExcludePath().iterator();
+
+                while (excludes.hasNext()) {
+                    String path = (String) excludes.next();
+                    excludePath.add(PathSelectors.ant(path));
+                }
+                Docket docket = (new Docket(DocumentationType.SWAGGER_2)).apiInfo(apiInfo).groupName(groupName).select().apis(RequestHandlerSelectors.basePackage(docketInfo.getBasePackage())).paths(Predicates.and(Predicates.not(Predicates.or(excludePath)), Predicates.or(basePath))).build();
+                configurableBeanFactory.registerSingleton(groupName, docket);
+                docketList.add(docket);
+            }
+
+            return docketList;
+        }
+    }
 }
--- battcn-framework-starter-swagger/src/main/java/com/battcn/framework/monitor/swagger/SwaggerProperties.java
+++ battcn-framework-starter-swagger/src/main/java/com/battcn/framework/monitor/swagger/SwaggerProperties.java
@@ -5,37 +5,252 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import com.battcn.framework.monitor.swagger.properties.ApiInfoProperties;
 import com.battcn.framework.monitor.swagger.properties.DocketProperties;
 
-@ConfigurationProperties(prefix = "swagger")
-public class SwaggerProperties implements java.io.Serializable {
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
-	private static final long serialVersionUID = 8471755917762607584L;
-	private Boolean enable;
-	private ApiInfoProperties apiInfo;
-	private DocketProperties docket;
+@ConfigurationProperties("swagger")
+public class SwaggerProperties {
+	private boolean enabled = false;
+	private String title = "";
+	private String description = "";
+	private String version = "";
+	private String license = "";
+	private String licenseUrl = "";
+	private String termsOfServiceUrl = "";
+	private SwaggerProperties.Contact contact = new SwaggerProperties.Contact();
+	private String basePackage = "";
+	private List<String> basePath = new ArrayList();
+	private List<String> excludePath = new ArrayList();
+	private Map<String, SwaggerProperties.DocketInfo> docket = new LinkedHashMap();
 
-	public ApiInfoProperties getApiInfo() {
-		return apiInfo;
+	public SwaggerProperties() {
 	}
 
-	public void setApiInfo(ApiInfoProperties apiInfo) {
-		this.apiInfo = apiInfo;
+	public boolean isEnabled() {
+		return this.enabled;
 	}
 
-	public DocketProperties getDocket() {
-		return docket;
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
 	}
 
-	public void setDocket(DocketProperties docket) {
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getLicense() {
+		return this.license;
+	}
+
+	public void setLicense(String license) {
+		this.license = license;
+	}
+
+	public String getLicenseUrl() {
+		return this.licenseUrl;
+	}
+
+	public void setLicenseUrl(String licenseUrl) {
+		this.licenseUrl = licenseUrl;
+	}
+
+	public String getTermsOfServiceUrl() {
+		return this.termsOfServiceUrl;
+	}
+
+	public void setTermsOfServiceUrl(String termsOfServiceUrl) {
+		this.termsOfServiceUrl = termsOfServiceUrl;
+	}
+
+	public SwaggerProperties.Contact getContact() {
+		return this.contact;
+	}
+
+	public void setContact(SwaggerProperties.Contact contact) {
+		this.contact = contact;
+	}
+
+	public String getBasePackage() {
+		return this.basePackage;
+	}
+
+	public void setBasePackage(String basePackage) {
+		this.basePackage = basePackage;
+	}
+
+	public List<String> getBasePath() {
+		return this.basePath;
+	}
+
+	public void setBasePath(List<String> basePath) {
+		this.basePath = basePath;
+	}
+
+	public List<String> getExcludePath() {
+		return this.excludePath;
+	}
+
+	public void setExcludePath(List<String> excludePath) {
+		this.excludePath = excludePath;
+	}
+
+	public Map<String, SwaggerProperties.DocketInfo> getDocket() {
+		return this.docket;
+	}
+
+	public void setDocket(Map<String, SwaggerProperties.DocketInfo> docket) {
 		this.docket = docket;
 	}
 
-	public Boolean getEnable() {
-		return enable;
+	public static class Contact {
+		private String name = "";
+		private String url = "";
+		private String email = "";
+
+		public String getName() {
+			return this.name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getUrl() {
+			return this.url;
+		}
+
+		public void setUrl(String url) {
+			this.url = url;
+		}
+
+		public String getEmail() {
+			return this.email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public Contact() {
+		}
 	}
 
-	public void setEnable(Boolean enable) {
-		this.enable = enable;
-	}
-	
+	public static class DocketInfo {
+		private String title = "";
+		private String description = "";
+		private String version = "";
+		private String license = "";
+		private String licenseUrl = "";
+		private String termsOfServiceUrl = "";
+		private SwaggerProperties.Contact contact = new SwaggerProperties.Contact();
+		private String basePackage = "";
+		private List<String> basePath = new ArrayList();
+		private List<String> excludePath = new ArrayList();
 
+		public String getTitle() {
+			return this.title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+		public String getDescription() {
+			return this.description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public String getVersion() {
+			return this.version;
+		}
+
+		public void setVersion(String version) {
+			this.version = version;
+		}
+
+		public String getLicense() {
+			return this.license;
+		}
+
+		public void setLicense(String license) {
+			this.license = license;
+		}
+
+		public String getLicenseUrl() {
+			return this.licenseUrl;
+		}
+
+		public void setLicenseUrl(String licenseUrl) {
+			this.licenseUrl = licenseUrl;
+		}
+
+		public String getTermsOfServiceUrl() {
+			return this.termsOfServiceUrl;
+		}
+
+		public void setTermsOfServiceUrl(String termsOfServiceUrl) {
+			this.termsOfServiceUrl = termsOfServiceUrl;
+		}
+
+		public SwaggerProperties.Contact getContact() {
+			return this.contact;
+		}
+
+		public void setContact(SwaggerProperties.Contact contact) {
+			this.contact = contact;
+		}
+
+		public String getBasePackage() {
+			return this.basePackage;
+		}
+
+		public void setBasePackage(String basePackage) {
+			this.basePackage = basePackage;
+		}
+
+		public List<String> getBasePath() {
+			return this.basePath;
+		}
+
+		public void setBasePath(List<String> basePath) {
+			this.basePath = basePath;
+		}
+
+		public List<String> getExcludePath() {
+			return this.excludePath;
+		}
+
+		public void setExcludePath(List<String> excludePath) {
+			this.excludePath = excludePath;
+		}
+
+		public DocketInfo() {
+		}
+	}
 }
